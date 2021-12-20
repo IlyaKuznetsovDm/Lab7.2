331067680c1da8018135bfcf004b7168e1c26123
Dmitr <Dmitr@DESKTOP-H2P4N61>
2021-12-20 18:34:58 +0200

    Добавьте файлы проекта.

---
+++ Lab7.2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //8 вариант - базовый уровень
+                Console.WriteLine("Введите предложение: ");
+                string TEXT1 = Console.ReadLine();
+            TEXT1 = TEXT1.Replace(' ', '~');
+                Console.WriteLine(TEXT1);
+            
+        }
+    }
+}
